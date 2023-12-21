e19c9af4b75fdc501f5e580432c5d34ec56d6685
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-12-21 00:37:50 -0500

    Add title and metadata, and Sass setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Portfolio Site
+# Portfolio Site
 ...
 
 ## Setup
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Aaren Arasaratnam",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "user-scalable=yes, width=device-width, initial-scale=1, maximum-scale=5" },
+        { name: "description", content: "..." }
+      ]
+    }
+  },
+  css: [
+    // ~ refers to home dir, @ refers to src dir
+    "/assets/scss/global.scss"
+  ]
 })
