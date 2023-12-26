f6e8db0ed4e0f79dbfc704572a9c147bbab74437
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-12-26 15:34:02 -0500

    Handle errors on error page

--- error.vue
+++ error.vue
@@ -1,8 +1,14 @@
 <template>
   <h1>oh wow whaddya know smth broke</h1>
+  <h2>{{ error.statusCode }}</h2>
+  <h2>{{ error.message }}</h2>
+  <button @click="handleError">Home</button>
 </template>
 
 <script setup>
+  defineProps(['error']);
+
+  const handleError = () => { clearError({ redirect: '/' }); }
 
 </script>
 
